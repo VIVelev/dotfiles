2f3ec15ff8f078820e465a1045319d0d4d0cef8e
Victor Velev <vivelev@icloud.com>
2022-05-12 02:51:51 -0400

    add impatiant/packer bootstrap

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,24 @@
-require('impatient')
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local pack_path = fn.stdpath("data") .. "/site/pack"
+local fmt = string.format
+
+local function ensure(user, repo)
+  -- Ensures a given github.com/USER/REPO is cloned in the pack/packer/start directory.
+  local install_path = fmt("%s/packer/start/%s", pack_path, repo)
+  if fn.empty(fn.glob(install_path)) > 0 then
+    execute(fmt("!git clone https://github.com/%s/%s %s", user, repo, install_path))
+    execute(fmt("packadd %s", repo))
+  end
+end
+
+-- Bootstrap essential plugins required for installing and loading the rest.
+ensure("wbthomason", "packer.nvim")
+ensure("lewis6991", "impatient.nvim")
+
+-- Load impatient which pre-compiles and caches Lua modules.
+require("impatient")
+
+-- Begin!
 require('vivelev')
